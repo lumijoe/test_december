e8fe26da170fd07db0af4037bba2a0fbb7402c0b
lumijoe <spacedesign63@gmail.com>
2023-12-20 23:52:44 +0900

    test push

--- pages/index.js
+++ pages/index.js
@@ -5,45 +5,76 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
+      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
+
+
+        {/* アイコンとタイトル */}
+        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <Image
+            src="/iconCat.png"
+            width={70}
+            height={70}
+            alt="image of app"
+          />
+        <div>
+          <code className="font-mono font-bold text-5xl ml-3">CashNyan</code>
+          <p className='ml-5'>キャッシュニャン</p></div>
+        </div>
+
+        {/* モバイル */}
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+
+          {/* モバイル：ヘッダー */}
+          <div className='relative'>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src="/mobile.png"
+              alt="mobileimage"
+              width={500}
+              height={500}
               priority
             />
-          </a>
+            <p className='text-sm text-white absolute top-[112px] left-[300px]'>12:12</p>
+            <div className='flex flex-row gap-0.5 absolute top-[110px] left-[350px]'>
+              <Image
+                src="/iconCellular.svg"
+                alt="image of cellular"
+                width={20}
+                height={20}
+              />
+              <Image
+                src="/iconWifi.svg"
+                alt="image of wifi"
+                width={20}
+                height={20}
+              />
+              <Image
+                src="/iconElectro.svg"
+                alt="image of electro"
+                width={20}
+                height={20}
+                style={{ rotate: "90deg" }}
+              />
+            </div>
+            <h2 className='text-4xl text-white absolute top-[182px] left-[60px]'>残高と支出入力</h2>
+
+            {/* モバイル：ヒーロー */}
+            <div>
+              
+            </div>
+          </div>
         </div>
+
+
+
+
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      {/* card/grid */}
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -116,3 +147,7 @@ export default function Home() {
     </main>
   )
 }
+/*
+半角&nbsp;
+
+*/
