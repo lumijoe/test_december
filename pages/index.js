0d02f659df934eac4fcb55ef77ef3c9665302bd4
lumijoe <spacedesign63@gmail.com>
2023-12-21 00:54:53 +0900

    test layout

--- pages/index.js
+++ pages/index.js
@@ -62,9 +62,125 @@ export default function Home() {
             <h2 className='text-4xl text-white absolute top-[182px] left-[60px]'>残高と支出入力</h2>
 
             {/* モバイル：ヒーロー */}
-            <div>
-              
-            </div>
+            {/* <div className='absolute top-[225px] left-[36.3px]'>
+              <Image
+                  src="/catHero2.jpg"
+                  alt="heroimage of cat"
+                  width={410.8}
+                  height={411}
+                />
+            </div> */}
+
+            {/* コンテンツ */}
+            {/* card/grid */}
+      <div className="mb-32 grid text-center lg:max-w-3xl w-[500px] lg:mb-0 lg:grid-cols-2 lg:text-left absolute top-[250px] left-[50px] gap-5">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent  pb-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 max-w-[180px]" 
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+          <Image
+              src="/catHero2.jpg"
+              alt="heroimage of cat"
+              width={200}
+              height={200}
+              className='rounded-t-lg'
+          />
+          <div className='mx-3'>
+          <h2 className={`mb-3 text-2xl`}>
+            December{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0  text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+          </div>
+        </a>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent  ml-[-58px] pb-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 max-w-[180px]" 
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+          <Image
+              src="/catHero2.jpg"
+              alt="heroimage of cat"
+              width={200}
+              height={200}
+              className='rounded-t-lg'
+          />
+          <div className='mx-3'>
+          <h2 className={`mb-3 text-2xl`}>
+            December{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0  text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+          </div>
+        </a>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent  pb-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 max-w-[180px]" 
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+          <Image
+              src="/catHero2.jpg"
+              alt="heroimage of cat"
+              width={200}
+              height={200}
+              className='rounded-t-lg'
+          />
+          <div className='mx-3'>
+          <h2 className={`mb-3 text-2xl`}>
+            December{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0  text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+          </div>
+        </a>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent ml-[-58px] pb-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 max-w-[180px]" 
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+          <Image
+              src="/catHero2.jpg"
+              alt="heroimage of cat"
+              width={200}
+              height={200}
+              className='rounded-t-lg'
+          />
+          <div className='mx-3'>
+          <h2 className={`mb-3 text-2xl`}>
+            December{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0  text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+          </div>
+        </a>
+
+        
+      </div>
+
+
+
+
           </div>
         </div>
 
@@ -74,76 +190,7 @@ export default function Home() {
 
       </div>
 
-      {/* card/grid */}
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   )
 }
